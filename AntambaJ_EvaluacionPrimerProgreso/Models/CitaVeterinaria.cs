fcd35c315b0898b42fc387ce913de21b70f570d7
JuanAntamba <antambajuan@gmail.com>
2025-05-01 08:03:22 -0500

    clase Propietario

---
+++ AntambaJ_EvaluacionPrimerProgreso/Models/CitaVeterinaria.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace AntambaJ_EvaluacionPrimerProgreso.Models
+{
+    public class CitaVeterinaria
+    {
+        
+    }
+}
---
+++ AntambaJ_EvaluacionPrimerProgreso/Models/Propietario.cs
@@ -0,0 +1,40 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace AntambaJ_EvaluacionPrimerProgreso.Models
+{
+    public class Propietario
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        [MinLength(3)]
+        public string Nombre { get; set; }
+
+        [Range(18, 100)]
+        public int Edad { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Correo { get; set; }
+
+        [Required]
+        public bool EsDueñoActivo { get; set; }
+
+        public string AntambaJ { get; set; } = "Antamba J";
+
+        [Required]
+        [DataType(DataType.Currency)]
+        [Range(0, 10000)]
+        public decimal DeudaPendiente { get; set; }
+
+        [Required]
+        [DataType(DataType.Date)]
+        public DateTime FechaRegistro { get; set; }
+    }
+}
+
+
+
