996364459d909c1847688f9a692be1f2f1661f4d
JuanAntamba <antambajuan@gmail.com>
2025-05-01 08:42:09 -0500

    Se cambio el metodo de cascada a restrict

---
+++ AntambaJ_EvaluacionPrimerProgreso/Migrations/20250501134055_Primera migracion.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AntambaJ_EvaluacionPrimerProgreso.Migrations
+{
+    /// <inheritdoc />
+    public partial class Primeramigracion : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Propietario",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nombre = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    Edad = table.Column<int>(type: "int", nullable: false),
+                    Correo = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    EsDueñoActivo = table.Column<bool>(type: "bit", nullable: false),
+                    AntambaJ = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    DeudaPendiente = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    FechaRegistro = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Propietario", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Mascota",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nombre = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    Raza = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    Edad = table.Column<int>(type: "int", nullable: false),
+                    Tipo = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    EstaVacunada = table.Column<bool>(type: "bit", nullable: false),
+                    PropietarioId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Mascota", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Mascota_Propietario_PropietarioId",
+                        column: x => x.PropietarioId,
+                        principalTable: "Propietario",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "CitaVeterinaria",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FechaVisita = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Motivo = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    RequiereMedicacion = table.Column<bool>(type: "bit", nullable: false),
+                    MascotaId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CitaVeterinaria", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_CitaVeterinaria_Mascota_MascotaId",
+                        column: x => x.MascotaId,
+                        principalTable: "Mascota",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CitaVeterinaria_MascotaId",
+                table: "CitaVeterinaria",
+                column: "MascotaId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Mascota_PropietarioId",
+                table: "Mascota",
+                column: "PropietarioId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "CitaVeterinaria");
+
+            migrationBuilder.DropTable(
+                name: "Mascota");
+
+            migrationBuilder.DropTable(
+                name: "Propietario");
+        }
+    }
+}
