ee44235677a243b80f55de43b17406a8e6ac3e81
JuanAntamba <antambajuan@gmail.com>
2025-05-01 08:31:41 -0500

    Clase mascota y citaVeterinaria

--- AntambaJ_EvaluacionPrimerProgreso/Models/CitaVeterinaria.cs
+++ AntambaJ_EvaluacionPrimerProgreso/Models/CitaVeterinaria.cs
@@ -4,7 +4,45 @@ using System.ComponentModel.DataAnnotations;
 namespace AntambaJ_EvaluacionPrimerProgreso.Models
 {
     public class CitaVeterinaria
+
     {
-        
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [DataType(DataType.Date)]
+        public DateTime FechaVisita { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        //En esta parte se puede poner las tres cosas que s nos pido vacunacion, revision general y cirugia
+        public string Motivo { get; set; }
+
+
+        [Required]
+        [Range(0, 1000)]
+        public decimal Tarifa
+        {
+            get
+            {
+                return Motivo.ToLower() switch
+                {
+                    "vacunacion" => 50,
+                    "revision general" => 20,
+                    "cirugia" => 100,
+                    _ => 0
+                };
+            }
+        }
+
+        [Required]
+        public bool RequiereMedicacion { get; set; }
+
+
+        public int MascotaId { get; set; }
+
+        [ForeignKey("MascotaId")]
+        public Mascota? Mascota { get; set; }
     }
+
 }
--- AntambaJ_EvaluacionPrimerProgreso/Models/Mascota.cs
+++ AntambaJ_EvaluacionPrimerProgreso/Models/Mascota.cs
@@ -5,6 +5,30 @@ namespace AntambaJ_EvaluacionPrimerProgreso.Models
 {
     public class Mascota
     {
-        
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string Nombre { get; set; } = string.Empty;
+
+        [Required]
+        [MaxLength(50)]
+        public string Raza { get; set; } = string.Empty;
+
+        [Required]
+        public int Edad { get; set; }
+
+        [Required]
+        public string Tipo { get; set; }
+
+        [Required]
+        public bool EstaVacunada { get; set; }
+
+        public int PropietarioId { get; set; }
+
+        [ForeignKey("PropietarioId")]
+        public Propietario? Propietario { get; set; }
+
     }
 }
