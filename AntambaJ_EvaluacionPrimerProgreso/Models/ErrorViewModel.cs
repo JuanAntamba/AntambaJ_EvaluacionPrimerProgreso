dbfaabfbf434882f84651b85eb700bcb7841ef3a
laboratorio <laboratorio@UPOAULA10702>
2025-05-01 07:18:32 -0500

    Agregar archivos de proyecto.

---
+++ AntambaJ_EvaluacionPrimerProgreso/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AntambaJ_EvaluacionPrimerProgreso.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
